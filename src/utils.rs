0156141b1974ff51f1b7570398696f5aa4dde05a
Xavi R. Pinteño <xavi@jrpinteno.me>
2023-12-04 22:11:22 +0200

    Added simple Grid struct

--- src/utils.rs
+++ src/utils.rs
@@ -32,3 +32,48 @@ pub struct Point {
 	pub x: u32,
 	pub y: u32
 }
+
+#[derive(Debug, Clone)]
+pub struct Grid {
+	data: Vec<char>,
+	rows: usize,
+	columns: usize
+}
+
+impl Grid {
+	pub fn new(rows: usize, columns: usize, default_value: char) -> Self {
+		let mut data = Vec::new();
+		data.resize(rows.checked_mul(columns).unwrap(), default_value);
+
+		Self {
+			data,
+			rows,
+			columns
+		}
+	}
+
+	fn get_index(&self, x: usize, y: usize) -> usize {
+		y * self.columns + x
+	}
+
+	fn get_value(&self, x: usize, y: usize) -> Option<char> {
+		if !(0 .. self.columns).contains(&y) || !(0 .. self.rows).contains(&x) {
+			None
+		}
+
+		let index = self.get_index(x, y);
+		self.data.get(index).copied()
+	}
+
+	fn set_value(&mut self, value: char, x: usize, y: usize) {
+		if x >= self.rows || y >= self.columns {
+			panic!("Coordinates don't exist ({}, {})", x, y);
+		}
+
+		let index = self.get_index(x, y);
+
+		if let Some(cell) = self.data.get_mut(index) {
+			*cell = value;
+		}
+	}
+}
