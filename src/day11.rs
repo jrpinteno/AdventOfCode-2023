15fb08f857ddced6cb1b80f21c6d4c33ffa46585
Xavi R. Pinteño <xavi@jrpinteno.me>
2023-12-11 21:16:59 +0200

    Day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -2,14 +2,14 @@ use aoc_2023::utils::grid::Grid;
 use aoc_2023::utils::point::Point;
 use aoc_2023::utils::utils;
 
-fn find_voids<T, F>(data: T) -> Vec<usize> where T: IntoIterator<Item = F>, F: AsRef<[char]> {
+fn find_voids<T, F>(data: T) -> Vec<i64> where T: IntoIterator<Item = F>, F: AsRef<[char]> {
 	data.into_iter()
 		.enumerate()
 		.filter_map(|(index, sequence)| {
 			if sequence.as_ref().iter().any(|&cell| cell != '.') {
 				None
 			} else {
-				Some(index)
+				Some(index as i64)
 			}
 		})
 		.collect()
@@ -19,8 +19,8 @@ fn expand_universe(universe: &mut Grid) {
 	let horizontal_voids = find_voids(universe.iter_rows());
 	let vertical_voids = find_voids(universe.iter_columns());
 
-	horizontal_voids.iter().rev().for_each(|&void_index| universe.insert_row_at(void_index, '.'));
-	vertical_voids.iter().rev().for_each(|&void_index| universe.insert_column_at(void_index, '.'));
+	horizontal_voids.iter().rev().for_each(|&void_index| universe.insert_row_at(void_index as usize, '.'));
+	vertical_voids.iter().rev().for_each(|&void_index| universe.insert_column_at(void_index as usize, '.'));
 }
 
 fn find_galaxies(universe: &Grid) -> Vec<Point> {
@@ -30,35 +30,74 @@ fn find_galaxies(universe: &Grid) -> Vec<Point> {
 			row.iter()
 				.enumerate()
 				.filter_map(move |(y, &ch)| (ch == '#')
-					.then(|| Point::new(x as i32, y as i32)))
+					.then(|| Point::new(x as i64, y as i64)))
 				.collect::<Vec<_>>()
 		})
 		.collect::<Vec<_>>()
 }
 
-fn get_galaxy_distances(universe: &Grid) -> Vec<i32> {
+fn get_galaxy_distances(universe: &Grid) -> Vec<u64> {
 	let galaxies = find_galaxies(universe);
 
-	let subtracted: Vec<Point> = galaxies.iter().enumerate()
+	galaxies.iter().enumerate()
 		.flat_map(|(i, &p1)| galaxies.iter().enumerate()
 			.skip(i + 1)
-			.map(move |(_, &p2)| p1 - p2 ))
-		.collect();
-
-	subtracted.iter().map(|distance| {
-		distance.x.abs() + distance.y.abs()
-	}).collect::<Vec<_>>()
+			.map(move |(_, &p2)| {
+				p1.manhattan_to(&p2)
+			}))
+		.collect()
 }
 
-fn main() {
-	let input = utils::read_file_to_vector("input/2023/day11.txt").unwrap();
+fn count_voids_between(points: &(i64, i64), voids: &Vec<i64>) -> u64 {
+	let (p1, p2) = points;
+	let (min_point, max_point) = (p1.min(&p2), p1.max(&p2));
+
+	voids.iter()
+		.filter(|&&x| {
+			(min_point .. max_point).contains(&&x)
+		})
+		.count() as u64
+}
+
+fn get_galaxy_distances_expansion(universe: &Grid, expansion_actor: u32) -> u64 {
+	let horizontal_voids = &find_voids(universe.iter_rows());
+	let vertical_voids = &find_voids(universe.iter_columns());
+	let galaxies = find_galaxies(&universe);
+
+	galaxies.iter().enumerate()
+		.flat_map(|(i, &p1)| galaxies.iter().enumerate()
+			.skip(i + 1)
+			.map(move |(_, &p2)| {
+				let h_voids_between = count_voids_between(&(p1.x, p2.x), &horizontal_voids);
+				let v_voids_between = count_voids_between(&(p1.y, p2.y), &vertical_voids);
+
+				p1.manhattan_to(&p2) + (h_voids_between + v_voids_between) * (expansion_actor as u64 - 1)
+			}))
+		.collect::<Vec<_>>().iter().fold(0, |acc, &x| acc + x as u64)
+}
+
+fn part_1(input: &Vec<String>) -> u64 {
 	let mut universe = Grid::from_string_vec(&input);
 	expand_universe(&mut universe);
 
 	let distances = get_galaxy_distances(&universe);
-	let distance_sum = distances.iter().fold(0, |acc, x| acc + x);
+	distances.iter().fold(0, |acc, x| acc + x)
+}
 
-	println!("Part 1: {}", distance_sum);
+fn part_2(input: &Vec<String>, expansion_factor: u32) -> u64 {
+	let universe = Grid::from_string_vec(&input);
+	let distance = get_galaxy_distances_expansion(&universe, expansion_factor);
+	distance
+}
+
+fn main() {
+	let input = utils::read_file_to_vector("input/2023/day11.txt").unwrap();
+	let result_1 = part_1(&input);
+
+	println!("Part 1: {}", result_1);
+
+	let result_2 = part_2(&input, 1_000_000);
+	println!("Part 2: {}", result_2);
 }
 
 #[cfg(test)]
@@ -145,4 +184,29 @@ mod tests {
 		let grid_distance_89 = galaxies[7] - galaxies[8];
 		assert_eq!(grid_distance_89.x.abs() + grid_distance_89.y.abs(), 5);
 	}
+
+	#[test]
+	fn test_find_distance_2() {
+		let input = SAMPLE_1.to_string_vector();
+		let result = part_2(&input, 2);
+
+		assert_eq!(result, 374);
+	}
+
+	#[test]
+	fn test_find_distance_10() {
+		let input = SAMPLE_1.to_string_vector();
+		let result = part_2(&input, 10);
+
+		assert_eq!(result, 1030);
+	}
+
+	#[test]
+	fn test_find_distance_100() {
+		let input = SAMPLE_1.to_string_vector();
+		let result = part_2(&input, 100);
+
+		assert_eq!(result, 8410);
+	}
+
 }
